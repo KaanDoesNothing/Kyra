fb6cbd11b620c7eb0004e77bbf92124e8171ab15
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-11 01:41:23 +0200

    Fix

--- src/commands/sendIntents.ts
+++
@@ -1,9 +0,0 @@
-import { ApplyOptions } from "@sapphire/decorators";
-import { Intents, Message } from "discord.js";
-import { Command, CommandOptions } from "@sapphire/framework";
-
-export class UserCommand extends Command {
-	public async run(msg: Message, args) {
-		return msg.channel.send({content: JSON.stringify(Intents.ALL)});
-	}
-}
