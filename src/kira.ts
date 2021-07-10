7ad0cb93641b837bd70d4599386d78635ec5e14b
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-11 01:35:19 +0200

    Does this fix the issue?

--- src/kira.ts
+++ src/kira.ts
@@ -13,7 +13,7 @@ const client = new SapphireClient({
 	listenOptions: {
 		port: 4000
     },
-	intents: Intents.NON_PRIVILEGED,
+	intents: Intents.ALL,
 	defaultPrefix: "=>",
 	caseInsensitiveCommands: true,
 	logger: {
