ca08b9ec5a5bf5b4e174909a8c44d1829b257c9e
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-11 12:28:57 +0200

    fixed the activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,5 +5,7 @@ import server from "../website/index";
 export class UserEvent extends Event<Events.Ready> {
 	public async run() {
         server(this.context.client);
+
+		this.context.client.user.setPresence({ activities: [{ name: `=>Help, =>Dashboard | ${this.context.client.guilds.cache.size} Servers`, url: "https://www.twitch.tv/vortexbot", type: 1 } ]});
 	}
 }
