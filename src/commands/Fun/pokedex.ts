f7dcd1c977e4bc0b030d26eec31a66fd01851105
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-19 21:21:57 +0200

    Added a pokedex

---
+++ src/commands/Fun/pokedex.ts
@@ -0,0 +1,46 @@
+import type { GuildMember, Message } from "discord.js";
+import { Args, CommandOptions, PermissionsPrecondition } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { EmbedConstructor } from "../../lib/embed";
+import { KiraCommand } from "../../lib/structures/command";
+import axios from "axios";
+
+@ApplyOptions<CommandOptions>({
+    aliases: ["pd"]
+})
+
+export class UserCommand extends KiraCommand {
+	public async run(msg: Message, args: Args) {
+        let input = await args.pick("string").catch(() => msg.reply({content: "Provide a pokemon name."}));
+        if(!input) return;
+
+        let pokemon: any = (await (await axios.get(`https://some-random-api.ml/pokedex?pokemon=${input}`)).data)
+
+        if(pokemon.error) return msg.reply(pokemon.error); 
+
+        let pokestats = [];
+
+        for (let stat in pokemon.stats) {
+            pokestats.push(pokemon.stats[stat]);
+        }
+
+        let embed = new EmbedConstructor()
+        .setTitle(pokemon.name)
+        .setDescription(pokemon.description)
+        .setThumbnail(pokemon.sprites.animated)
+        .addField("ID", pokemon.id, true)
+        .addField("Type", pokemon.type.join(", "))
+        .addField("Species", pokemon.species.join(", "), true)
+        .addField("Abilities", pokemon.abilities.join(", "), true)
+        .addField("Height", pokemon.height, true)
+        .addField("Weight", pokemon.weight, true)
+        .addField("Base Experience", pokemon.base_experience, true)
+        .addField("Gender", pokemon.gender.join(", "), true)
+        .addField("Egg Groups", pokemon.egg_groups.join(", "), true)
+        .addField("Stats", pokestats.join(", "), true)
+        .addField("Evolutions", pokemon.family.evolutionLine.join(", "), true)
+        .addField("Generation", pokemon.generation, true);
+
+        msg.channel.send({embeds: [embed]});
+	}
+}
