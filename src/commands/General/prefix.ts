a533a466433480d74ebaef128aa1d6dc25d36b51
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-22 02:17:19 +0200

    changed the prefix command so it uses the new provider

--- src/commands/General/prefix.ts
+++ src/commands/General/prefix.ts
@@ -2,7 +2,7 @@ import { ApplyOptions } from "@sapphire/decorators";
 import type { Message } from "discord.js";
 import { CommandOptions, Args } from "@sapphire/framework";
 import { PermissionsPrecondition } from "@sapphire/framework";
-import { db, getGuild, updateGuild } from "../../lib/db";
+import { provider } from "../../lib/db";
 import { KiraCommand } from "../../lib/structures/command";
 
 @ApplyOptions<CommandOptions>({
@@ -17,11 +17,11 @@ export class UserCommand extends KiraCommand {
 
 		if (prefix.length < 1) return msg.reply("You must provide a prefix.");
 
-		let guildData = await getGuild(msg.guild.id);
+		let guildData = await provider.get("guilds", msg.guild.id);
 
 		guildData.prefix = prefix;
 
-		await updateGuild(guildData);
+		await provider.set("guilds", msg.guild.id, guildData);
 
 		return msg.reply(`Prefix has been set to ${prefix}.`);
 	}
