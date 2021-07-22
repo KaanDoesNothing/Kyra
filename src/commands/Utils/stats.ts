b9143d6368b24afcdd6ffc808d66bb3b8ca162d5
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-22 17:04:18 +0200

    Updated the stats command

--- src/commands/Utils/stats.ts
+++ src/commands/Utils/stats.ts
@@ -1,11 +1,11 @@
-import { ApplyOptions } from "@sapphire/decorators";
 import type { Message } from "discord.js";
-import { CommandOptions, Args } from "@sapphire/framework";
-import si from "systeminformation";
+import { Args } from "@sapphire/framework";
 import { version } from "discord.js";
 import { EmbedConstructor } from "../../lib/embed";
 import { commandsCache } from "../../lib/cache";
 import { KiraCommand } from "../../lib/structures/command";
+import {totalmem, freemem, uptime} from "os";
+import { millisToMinutesAndSeconds } from "../../lib/utils";
 
 export class UserCommand extends KiraCommand {
 	public async run(msg: Message, args: Args) {
@@ -20,6 +20,9 @@ export class UserCommand extends KiraCommand {
             let guilds = this.context.client.guilds.cache.size;
             let channels = this.context.client.channels.cache.size;
             let users = this.context.client.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0);
+
+            let totalMem = totalmem() / 1024 / 1024 / 1024;
+            let freeMem = freemem() / 1024 / 1024 / 1024;
             
             let embed = new EmbedConstructor(msg)
             .addField("Owner", this.context.client.users.cache.get(ownerApplication.owner.id).tag, true)
@@ -27,10 +30,13 @@ export class UserCommand extends KiraCommand {
             .addField("Commands", commands.toString(), true)
             .addField("Commands Ran", commandsCache.commands_ran.toString(), true)
             .addField("Shards", shardCount.toString(), true)
+            .addField("Uptime", millisToMinutesAndSeconds(this.context.client.uptime), true)
             .addField("Guilds", guilds.toString(), true)
             .addField("Channels", channels.toString(), true)
             .addField("Users", users.toString(), true)
-            .addField("OS", os, true);
+            .addField("OS", os, true)
+            // .addField("OS Uptime", millisToMinutesAndSeconds(uptime));
+            // .addField("Total Ram", `${(totalMem - freeMem).toFixed(2)} GB / ${totalMem} GB`, true)
 
             return msg.channel.send({embeds: [embed]});
 	}
