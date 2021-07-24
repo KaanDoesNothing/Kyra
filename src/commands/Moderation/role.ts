76000734ca2411d7fcb7f8ab96a70a78b8bf0988
KaanDoesNothing <kaanyt123@gmail.com>
2021-07-24 18:43:55 +0200

    Hm

--- src/commands/Moderation/role.ts
+++ src/commands/Moderation/role.ts
@@ -10,11 +10,14 @@ import { KiraCommand, KiraCommandOptions } from "../../lib/structures/command";
 
 export class UserCommand extends KiraCommand {
 	public async add(msg: Message, args: Args) {
-        let member: GuildMember = await args.pick("member");
-        let role: Role = await args.pick("role");
+        let member_arg = await args.pickResult("member");
+        if(member_arg.error) return msg.reply({content: "You need to mention someone."});
+        let member = member_arg.value;
 
-        let reason = await args.rest("string");
-
+        let role_arg = await args.pickResult("role");
+        if(role_arg.error) return msg.reply({content: "You need to mention someone."});
+        let role = role_arg.value;
+        
         if(!msg.member.permissions.has("MANAGE_ROLES")) return msg.reply({content: `You don't have the permissions to do this.`});
         
         if(member.roles.cache.has(role.id)) return msg.reply(`Member already has the following role: ${role.name}.`);
@@ -23,10 +26,13 @@ export class UserCommand extends KiraCommand {
 	}
 
     public async remove(msg: Message, args: Args) {
-        let member: GuildMember = await args.pick("member");
-        let role: Role = await args.pick("role");
+        let member_arg = await args.pickResult("member");
+        if(member_arg.error) return msg.reply({content: "You need to mention someone."});
+        let member = member_arg.value;
 
-        let reason = await args.rest("string");
+        let role_arg = await args.pickResult("role");
+        if(role_arg.error) return msg.reply({content: "You need to mention someone."});
+        let role = role_arg.value;
 
         if(!msg.member.permissions.has("MANAGE_ROLES")) return msg.reply({content: `You don't have the permissions to do this.`});
         
