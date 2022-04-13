a71e402aaa94ed6ed514703f45d99a05eef72d6c
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-13 15:33:42 +0200

    Added the ability to view the warnings for a user

---
+++ src/commands/Moderation/warns.ts
@@ -0,0 +1,40 @@
+import type { GuildMember, Message, User } from "discord.js";
+import {Args, CommandOptions, isErr, Result, UserError, UserPermissionsPrecondition} from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { KiraCommand } from "../../lib/structures/command";
+import {GuildWarning} from "../../entities/guildWarning";
+import locale from "../../lib/locale";
+import {EmbedConstructor} from "../../lib/embed";
+
+@ApplyOptions<CommandOptions>({
+    preconditions: [new UserPermissionsPrecondition("MANAGE_MESSAGES")]
+})
+
+export class UserCommand extends KiraCommand {
+    public async messageRun(msg: Message, args: Args) {
+        let member: Result<GuildMember, UserError> = await args.pickResult("member");
+
+        if(isErr(member)) {
+            return msg.reply(locale.commands.args.user);
+        }
+
+        let warnings = await GuildWarning.find({where: {guild_id: msg.guild.id, user_id: member.value.user.id}, order: {createdAt: "DESC"}});
+
+        let embed = new EmbedConstructor()
+            .setTitle("Warnings for " + member.value.user.tag);
+
+        let description = "";
+
+        for (let i in warnings) {
+            let warning = warnings[i];
+
+            let author = await this.container.client.users.fetch(warning.author_id).catch(err => console.log("Couldn't fetch user."));
+
+            description += `${new Date(warning.createdAt).toLocaleString()}, ${(author as User).tag}: ${warning.reason}\n`
+        }
+
+        embed.setDescription(description);
+
+        return msg.channel.send({embeds: [embed]});
+    }
+}
